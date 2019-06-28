9290a6452f13a402a2fd5472b9e1f407ee112d7f
Genevieve <genevieve.carter@hotmail.com>
2019-06-29 00:31:36 +1000

    Added basic fetch functionality and API key

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import React from 'react'
 import './App.css';
 
 let baseClass = "mdb-app";
+let apiKey = "6ed12e064b90ae1290fa326ce9e790ff";
+let movieResults = [];
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // error: null,
+      // isLoaded: false,
+      movieResults: []
+    };
+  }
+
+  componentDidMount() {
+    var movieQuery = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key="
+    var movieString = movieQuery.concat(apiKey);
+    fetch(movieString)
+      .then(res => res.json())
+      .then(function(data) {
+        movieResults = data.results;
+        console.log(movieResults);
+      })
+      .catch(error => console.log('Error is ', error));
+  }
+
   render() {
     return (
       <div className={baseClass}>
@@ -18,6 +41,8 @@ class App extends React.Component {
           >
           </input>
         </form>
+        <div className={`${baseClass}__movie-container`}>
+        </div>
       </div>
     )
   }
