b94d9b723c750d61c24850bb4c99988fb5ae396d
Genevieve <genevieve.carter@hotmail.com>
2019-06-30 12:05:32 +1000

    Added hero background overlay to home page

--- src/components/hero-header/hero-header.js
+++ src/components/hero-header/hero-header.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import Logo from '../../../src/movie-db-logo.svg';
+import HeroBackground from '../../../src/movie_db_hero_bg_large.png';
 import './hero-header.css';
 
 const baseClass = "mdb-hero-header";
 
 class HeroHeader extends React.Component {
   render() {
+    var divStyle = {
+      backgroundImage: 'url(' + HeroBackground + ')',
+    };
+
     return (
+      <>
+        <div className={`${baseClass}__overlay`} style={divStyle}>
+        </div>
         <header className={`${baseClass}`}>
             <img className={`${baseClass}__logo`} src={Logo} alt="Movie DB Logo"></img>
             <form className={`${baseClass}__search-form`}>
                 {this.props.children}
             </form>
         </header>
-        )
+      </>
+    )
   }
 }
 
